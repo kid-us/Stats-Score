c3d9ee9d7c8114cbc39a21e5268ec6abc0e2e0dc
kid-us <kidushh29@gmail.com>
2025-12-13 00:08:03 +0300

    env fix

--- api/competition.ts
+++ api/competition.ts
@@ -8,8 +8,11 @@ export default async function handler(req: any, res: any) {
       return res.status(400).json({ error: "Missing competition or matchDay" });
     }
 
-    const token = process.env.VITE_ACCESS_TOKEN;
-    const baseUrl = process.env.VITE_API_URL;
+    // const token = process.env.VITE_ACCESS_TOKEN;
+    // const baseUrl = process.env.VITE_API_URL;
+
+    const token = process.env.API_TOKEN;
+    const baseUrl = process.env.API_URL;
 
     const response = await axios.get(
       `${baseUrl}/competitions/${competition}/matches`,
